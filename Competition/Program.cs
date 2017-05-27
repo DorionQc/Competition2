b29bffbd59682f0dbbe275d8bace3ff1651965f8
clonecharle1 <charl_i_c@hotmail.com>
2017-05-26 20:45:00 -0400

    Changed Name

--- Competition/Program.cs
+++ Competition/Program.cs
@@ -13,7 +13,7 @@ namespace Competition
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new RobotWar())
                 game.Run();
         }
     }
--- Competition/RobotWar.cs
+++ Competition/RobotWar.cs
@@ -7,12 +7,12 @@ namespace Competition
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class RobotWar : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         
-        public Game1()
+        public RobotWar()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
