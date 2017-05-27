3ff8384560ae413fc2058b094bde57fa24b5f62c
clonecharle1 <charl_i_c@hotmail.com>
2017-05-26 23:07:13 -0400

    Working on entity and weapon

---
+++ Competition/Armes/Pistolet.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Competition.Armes
+{
+    class Pistolet : OldArme
+    {
+        private bool m_PeutTirer;
+        public Pistolet(int pCapaciteChargeur, int pQuantiteTotalBalles) :base(pCapaciteChargeur,pQuantiteTotalBalles)
+        {
+            m_PeutTirer = true;
+        }
+
+        public override bool FullAuto
+        {
+            get { return false; }
+        }
+
+        public override void Update(GameTime gameTime, Vector2 Orientation)
+        {
+            if (IsShooting && m_PeutTirer)
+            {
+                m_PeutTirer = false;
+                IsShooting = false;
+                if (gameTime.TotalGameTime.TotalMilliseconds - m_TempsDernierTir > m_TempsEntreChaqueTir)
+                {
+                    if (QuantiteBallesDansChargeur > 0)
+                    {
+                        Tirer(Orientation);
+                        QuantiteBallesDansChargeur--;
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+            }
+        }
+
+        public override void CommenceATirer(GameTime gameTime)
+        {
+            IsShooting = true;
+        }
+
+        public override void FiniDeTirer(GameTime gameTime)
+        {
+            m_PeutTirer = true;
+        }
+
+        private void Tirer(Vector2 Orientation)
+        {
+            //TODO Implement shooting mecanics
+
+
+        }
+
+        
+        
+    }
+}
---
+++ Competition/Entity/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Competition.Armes;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Competition.Entity
+{
+    class Player : Entity
+    {
+        private Weapons[] m_WeaponList;
+        public Player(Texture2D[] EntityTextures, Vector2 StartPosition, Vector2 StartSize, Vector2 StartVelocity, Weapons[] pWeapons, double AnimationTimerStart = 0.0,
+            double AnimationTimerDuration = 1000.0/*millisecondes*/, bool AnimationLoop = true) :base(EntityTextures, StartPosition, StartSize, StartVelocity, AnimationTimerStart, AnimationTimerDuration, AnimationLoop)
+        {
+            m_WeaponList = new Weapons[1];
+            m_WeaponList[0] = new Pistol();
+        }
+
+    }
+}
