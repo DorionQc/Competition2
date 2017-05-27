3628fa75519d5424d44d671c7edd2c95621f6f3f
DorionQc <gouletsamuel@hotmail.com>
2017-05-26 22:58:07 -0400

    Merge branch 'master' of https://github.com/DorionQc/Competition2

--- Competition/TextureManager.cs
+++ Competition/TextureManager.cs
@@ -13,12 +13,24 @@ namespace Competition
     public static class TextureManager
     {
         public static Texture2D TextureBarricade;
-        public static Texture2D TextureTerre;
+        public static Texture2D[] TextureTerre;
 
         public static void init(ContentManager Content)
         {
+            //Environnement
             TextureBarricade = Content.Load<Texture2D>("Textures/TileBarricade");
+<<<<<<< HEAD
             TextureTerre = Content.Load<Texture2D>("Textures/wood");
+=======
+
+            //Arrière-Plan
+            TextureTerre = new Texture2D[2];
+            TextureTerre[0] = Content.Load<Texture2D>("Textures/TileTerre1.png");
+            TextureTerre[1] = Content.Load<Texture2D>("Textures/TileTerre2.png");
+
+            //Personnages
+
+>>>>>>> 14d1bb2bfb60a74b14c1d0aae9048b85f3265337
         }
     }
 }
