fb06d37d6026ac565e0a5ae245fc0eecf098a04d
DorionQc <gouletsamuel@hotmail.com>
2017-05-26 21:40:29 -0400

    Penumbra working

--- Competition/Jeu/Tiles/TileTerre.cs
+++ Competition/Jeu/Tiles/TileTerre.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Competition.Jeu.Tiles
 {
-    class TileTerre
+    public class TileTerre : Tile
     {
+        public TileTerre(int x, int y, Map parent) : base(x, y, parent, false, true, true)
+        { }
+
+        public override Texture2D Texture
+        {
+            get
+            {
+                return TextureManager.TextureTerre;
+            }
+        }
+
+        public override TileType Type
+        {
+            get
+            {
+                return TileType.Terre;
+            }
+        }
     }
 }
