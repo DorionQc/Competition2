22c6d3b82b0a1275b08c566117f1947a1d720af1
DorionQc <gouletsamuel@hotmail.com>
2017-05-26 21:06:43 -0400

    Maps et Tiles

---
+++ Competition/Jeu/Tiles/Tile.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+
+namespace Competition.Jeu.Tiles
+{
+    /*
+    public abstract class Tile
+    {
+        protected int m_x;
+        protected int m_y;
+
+        protected Map m_Map;
+
+        protected bool m_Breakable;
+
+        protected bool m_Breaking;
+        protected bool m_FireGoThrough;
+
+        protected bool m_Solid;
+
+        protected Fire m_FireEntity;
+
+        public AbsCase(int x, int y, Map m, bool Solid, bool Breakable, bool LetsFireThrough)
+        {
+            m_x = x;
+            m_y = y;
+            m_Map = m;
+            m_Solid = Solid;
+            m_Breakable = Breakable;
+            m_Breaking = false;
+            m_FireGoThrough = LetsFireThrough;
+        }
+
+        public int X
+        {
+            get { return m_x; }
+        }
+
+        public int Y
+        {
+            get { return m_y; }
+        }
+
+        public Map Parent
+        {
+            get { return m_Map; }
+        }
+
+        public bool IsBreakable
+        {
+            get { return m_Breakable; }
+        }
+
+        public bool IsBreaking
+        {
+            get { return m_FireEntity != null; }
+        }
+
+        public Fire Fire
+        {
+            get { return m_FireEntity; }
+            set
+            {
+                m_FireEntity = value;
+            }
+        }
+
+        public bool LetsFireThrough
+        {
+            get { return m_FireGoThrough; }
+        }
+
+        public virtual bool IsSolid
+        {
+            get { return m_Solid; }
+        }
+
+        public abstract Image Texture { get; }
+
+        public abstract CaseType Type { get; }
+
+        public virtual void Draw(Graphics g, Rectangle r, SolidBrush b, float Width)
+        {
+            g.DrawImage(Texture, m_x * Width, m_y * Width, Width, Width);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} at {1}, {2}", Type, m_x, m_y);
+        }
+    }
+    */
+}
