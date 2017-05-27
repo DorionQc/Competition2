c5de590fca326035bbee5471e6e0f862363a40fc
DorionQc <gouletsamuel@hotmail.com>
2017-05-26 20:51:21 -0400

    IPartieDeJeu

---
+++ Competition/IPartieDeJeu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Competition
+{
+    public interface IPartieDeJeu
+    {
+        void Draw(GameTime gameTime);
+        void Update(GameTime gameTime);
+    }
+}
