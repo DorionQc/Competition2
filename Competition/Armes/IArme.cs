351f7f8aebe44245ad40bf8f673227b9d7f5aae4
clonecharle1 <charl_i_c@hotmail.com>
2017-05-26 21:20:47 -0400

    Working of IArme et gitignore

--- Competition/Armes/IArme.cs
+++ Competition/Armes/IArme.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace Competition.Armes
 {
     interface IArme
     {
-        int MagasineCapacity
+        int CapaciteChargeur
         {
-            get; set;
+            get;
+            set;
         }
+
+        int QuantiteBallesDansChargeur
+        {
+            get;
+            set;
+        }
+
+        int QuantiteTotaleBalles
+        {
+            get;
+            set;
+        }
+
+        void Update(GameTime gameTime);
+        void CommenceATirer(GameTime gameTime);
+        void FiniDeTirer(GameTime gameTime);
+
 
 
     }
